a41b80f3526641a2c84727d0127ec1566a3a506e
doniellekinchen <donicodes.things@gmail.com>
2024-01-10 10:49:47 -0500

    finish collecting cats

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Cat
 
 # Add this cats list below the imports
 cats = [
@@ -17,7 +18,12 @@ def about(request):
     return render(request, 'about.html')
 
 def cats_index(request):
+  cats = Cat.objects.all()
   # We pass data to a template very much like we did in Express!
   return render(request, 'cats/index.html', {
     'cats': cats
   })
+
+def cats_detail(request, cat_id):
+  cat = Cat.objects.get(id=cat_id)
+  return render(request, 'cats/detail.html', { 'cat': cat })
